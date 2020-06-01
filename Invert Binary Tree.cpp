054cbaf1ac3fc81d3eb1f8ad23fffff13ec5b109
Jaskaran Batra <jskrnbtr@gmail.com>
2020-06-01 13:03:35 +0530

    Create Invert Binary Tree.cpp
    Invert a binary tree.
    
    Example:
    
    Input:
    
         4
       /   \
      2     7
     / \   / \
    1   3 6   9
    Output:
    
         4
       /   \
      7     2
     / \   / \
    9   6 3   1

---
+++ Invert Binary Tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root){
+            return NULL;
+        }
+        root->left=invertTree(root->left);
+        root->right=invertTree(root->right);
+        TreeNode * temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        return root;
+    }
+};
+
+
+
+//Other solution (Java)
+public TreeNode invertTree(TreeNode root) {
+    if (root == null) return null;
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+        TreeNode current = queue.poll();
+        TreeNode temp = current.left;
+        current.left = current.right;
+        current.right = temp;
+        if (current.left != null) queue.add(current.left);
+        if (current.right != null) queue.add(current.right);
+    }
+    return root;
+}
